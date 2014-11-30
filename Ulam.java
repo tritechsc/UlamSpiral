f257f00143ad0b69ad808fc0ceb33e1844d2ba15
Craig Coleman <iuomo@users.noreply.github.com>
2014-11-30 07:22:18 -0800

    Create Ulam.java

---
+++ Ulam.java
@@ -0,0 +1,28 @@
+class Ulam {
+	public static void main (String args[]){
+		int x = 0, y = 0,  e; // e is the end state of nlogic;
+		int h = 0, k = 0;
+		int parity = -1;//parity 0 + 1 
+		int parityMOD = 4;
+		int n, nlogic =0;
+		n = 1;
+		System.out.println("n:"+n+" x:"+x+" y:"+y+" h:"+h+" k:"+k+" nlogic "+nlogic+" parity:"+parity);
+		for (e = 1;e < 4;e++){
+			parity = -parity;
+			System.out.println (e + " e ***************************");
+			nlogic = 0;
+			for (x = 0; x < e; x++){
+				h = h + parity;
+					n++;
+				System.out.println("n:"+n+" x:"+x+" y:"+y+" h:"+h+" k:"+k+" nlogic "+nlogic+" parity:"+parity);
+		}
+			nlogic =1;
+			for (y = 0; y < e; y++){
+					k = k + parity;
+					n++;
+		System.out.println("n:"+n+" x:"+x+" y:"+y+" h:"+h+" k:"+k+" nlogic "+nlogic+" parity:"+parity);
+
+			}
+		}
+	}
+}
